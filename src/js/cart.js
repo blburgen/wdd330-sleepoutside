86379109ca9e8de9af8543aa1c45dc0e74f8a229
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-03-01 17:38:35 -0600

    debug adding tent to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,9 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  let cartItems = getLocalStorage("so-cart");
+  cartItems = JSON.parse(cartItems);
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let currentLocal = getLocalStorage("so-cart");
+
+  if (currentLocal) {
+    let cart = JSON.parse(currentLocal);
+    cart.push(product);
+    cart = JSON.stringify(cart);
+    setLocalStorage("so-cart", cart);
+  } else {
+    let cart = [];
+    cart.push(product);
+    cart = JSON.stringify(cart);
+    setLocalStorage("so-cart", cart);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
