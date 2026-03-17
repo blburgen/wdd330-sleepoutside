aa4d7c3d859b716b380e104f54e9f50ab70f3256
SchoolTechBytes <nicholas.j.nixon.27@outlook.com>
2026-03-16 22:12:05 -0500

    Week 03 start of team work

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,46 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+  let hasDiscount = false;
+  let discountAmount = 0;
+  if (item.FinalPrice < item.SuggestedRetailPrice) {
+    hasDiscount = true;
+    discountAmount = Math.round(
+      (item.FinalPrice / item.SuggestedRetailPrice) * 100 - 100,
+    );
+  }
+  const newItem = `
+        <li class="product-card">
+            <a href="product_pages/?product=${item.Id}">
+                <img
+                src=${item.Image}
+                alt=${item.Name}
+              />
+                <h3 class="card__brand">${item.Name}</h3>
+                <h2 class="card__name">${item.NameWithoutBrand}</h2>
+                <div class="container_price">
+                  <p class="product-card__price">$${item.FinalPrice}</p>
+                  ${hasDiscount ? `<p class="discount__price">${discountAmount}% off</p>` : ""}
+                </div>
+            </a>
+        </li>`;
+
+  return newItem;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,54 @@
-import { getTentData } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const productListing = document.querySelector(".product-list");
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+const productList = new ProductList("tents", dataSource, listElement);
+productList.init();
 
-async function renderCartContents() {
-  let tentData = await getTentData();
-  const htmlItems = tentData.map((item) =>
-    item.ProductPageUrl ? cartItemTemplate(item) : null,
-  );
-  productListing.innerHTML = htmlItems.join("");
-}
 
-function cartItemTemplate(item) {
-  let hasDiscount = false;
-  let discountAmount = 0;
-  if (item.FinalPrice < item.SuggestedRetailPrice) {
-    hasDiscount = true;
-    discountAmount = Math.round(
-      (item.FinalPrice / item.SuggestedRetailPrice) * 100 - 100,
-    );
-  }
-  const newItem = `
-        <li class="product-card">
-            <a href="product_pages/?product=${item.Id}">
-                <img
-                src=${item.Image}
-                alt=${item.Name}
-              />
-                <h3 class="card__brand">${item.Name}</h3>
-                <h2 class="card__name">${item.NameWithoutBrand}</h2>
-                <div class="container_price">
-                  <p class="product-card__price">$${item.FinalPrice}</p>
-                  ${hasDiscount ? `<p class="discount__price">${discountAmount}% off</p>` : ""}
-                </div>
-            </a>
-        </li>`;
 
-  return newItem;
-}
 
-renderCartContents();
+// import { getTentData, loadHeaderFooter } from "./utils.mjs";
+
+// const productListing = document.querySelector(".product-list");
+
+// async function renderCartContents() {
+//   let tentData = await getTentData();
+//   const htmlItems = tentData.map((item) =>
+//     item.ProductPageUrl ? cartItemTemplate(item) : null,
+//   );
+//   productListing.innerHTML = htmlItems.join("");
+// }
+
+// function cartItemTemplate(item) {
+//   let hasDiscount = false;
+//   let discountAmount = 0;
+//   if (item.FinalPrice < item.SuggestedRetailPrice) {
+//     hasDiscount = true;
+//     discountAmount = Math.round(
+//       (item.FinalPrice / item.SuggestedRetailPrice) * 100 - 100,
+//     );
+//   }
+//   const newItem = `
+//         <li class="product-card">
+//             <a href="product_pages/?product=${item.Id}">
+//                 <img
+//                 src=${item.Image}
+//                 alt=${item.Name}
+//               />
+//                 <h3 class="card__brand">${item.Name}</h3>
+//                 <h2 class="card__name">${item.NameWithoutBrand}</h2>
+//                 <div class="container_price">
+//                   <p class="product-card__price">$${item.FinalPrice}</p>
+//                   ${hasDiscount ? `<p class="discount__price">${discountAmount}% off</p>` : ""}
+//                 </div>
+//             </a>
+//         </li>`;
+
+//   return newItem;
+// }
+
+
+// renderCartContents();
+loadHeaderFooter();
