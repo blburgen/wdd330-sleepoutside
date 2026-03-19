5f2034ef3c90e60b505591c437d7f5f7a6e17a85
Brady Burgener <bradylbsee@gmail.com>
2026-03-19 11:49:01 -0600

    lint and format

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -4,7 +4,7 @@ import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParam('category');
+const category = getParam("category");
 const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList(category, dataSource, listElement);
