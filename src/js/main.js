015ceb9c7657ece75d7df9591ae930d4728ea48c
Brady Burgener <bradylbsee@gmail.com>
2026-03-19 09:00:25 -0600

    cart number count

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,3 @@ const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, listElement);
 productList.init();
-
-
