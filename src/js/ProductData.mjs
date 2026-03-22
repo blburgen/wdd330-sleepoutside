454a5d028876a5b41b5aedd710b5881d782b49fd
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-03-21 20:20:58 -0500

    allow to run locally

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,26 +1,36 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL =
+    import.meta.env.VITE_SERVER_URL
 
 function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
+    if (res.ok) {
+        return res.json();
+    } else {
+        throw new Error("Bad Response");
+    }
 }
 
 export default class ProductData {
-  constructor() {
-  }
+    constructor() {}
 
-  async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category} `);
-    const data = await convertToJson(response);
-    return data.Result;
-  }
+    async getData(category) {
+        try {
+            const response = await fetch(`${baseURL}products/search/${category}`);
+            const data = await convertToJson(response);
+            return data.Result;
+        } catch (err) {
+            const res = await fetch(`/json/${category}.json`);
+            const data = await res.json();
+            const list = Array.isArray(data) ? data : data.Result || [];
+            return list.map((item) =>
+                item.Images && item.Images.PrimaryLarge ?
+                item : {...item, Images: { PrimaryLarge: item.Image } }
+            );
+        }
+    }
 
-  async findProductById(id) {
-    const response = await fetch(`${baseURL}product/${id}`);
-    const data = await convertToJson(response);
-    return data.Result;
-  }
+    async findProductById(id) {
+        const response = await fetch(`${baseURL}product/${id}`);
+        const data = await convertToJson(response);
+        return data.Result;
+    }
 }
