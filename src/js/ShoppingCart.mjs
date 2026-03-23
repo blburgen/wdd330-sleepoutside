23d1f21219462f8d4ba7244629832d85c7bddd3a
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-03-23 01:44:19 -0500

    quantity counter

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -2,88 +2,6 @@ import { getLocalStorage, setLocalStorage, renderListWithTemplate, updateCartCou
 
 function cartItemTemplate(item) {
 
-    // let quantity = 1
-    // let itemTotalPrice = item.FinalPrice
-    // sessionStorage.setItem("quantity",quantity )
-    // sessionStorage.setItem("total",)
-    // //! Convert this do obj to access coresponding btns to update quantity
-    // const li = document.createElement("li")
-    // const removeBtn = document.createElement("button")
-    // const imageLink = document.createElement("a")
-    // const itemImage = document.createElement("img")
-    // const itemNameLink = document.createElement("a")
-    // const itemName = document.createElement("h2")
-    // const colorDescription = document.createElement("p")
-    // const quantityControlsDiv = document.createElement("div")
-    // const increaseBtn = document.createElement("btn")
-    // const quantityOfItems = document.createElement("p")
-    // const decreaseBtn = document.createElement("btn")
-    // const itemPrice = document.createElement("p")
-
-    // li.appendChild(removeBtn)
-
-    // imageLink.appendChild(itemImage)
-    // li.appendChild(imageLink)
-
-    // itemNameLink.appendChild(itemName)
-    // li.appendChild(itemNameLink)
-
-    // li.appendChild(colorDescription)
-    // quantityControlsDiv.appendChild(decreaseBtn)
-    // quantityControlsDiv.appendChild(quantityOfItems)
-    // quantityControlsDiv.appendChild(increaseBtn)
-    // li.appendChild(quantityControlsDiv)
-    // li.appendChild(itemPrice)
-
-    // li.classList.add("cart-card")
-    // li.classList.add("divider")
-
-    // removeBtn.classList.add("cart-card__remove")
-    // removeBtn.type = "button"
-    // removeBtn.setAttribute("data-id",item.id)
-    // removeBtn.setAttribute("aria-label",`Remove ${item.Name} from cart`)
-    // removeBtn.textContent = "X"
-
-    // imageLink.classList.add("cart-card__image")
-    // imageLink.setAttribute("href","#")
-    // itemImage.setAttribute("src",item.Images.PrimaryLarge)
-    // itemImage.setAttribute("alt",item.Name)
-
-    // itemNameLink.setAttribute("href","#")
-    // itemName.classList.add("card__name")
-    // itemName.textContent = item.Name
-
-    // colorDescription.classList.add("cart-card__color")
-    // colorDescription.textContent = item.Colors[0].ColorName
-
-    // quantityControlsDiv.classList.add("cart-card__quantity-controls")
-
-    // decreaseBtn.classList.add("cart-card__quantity-control_btn")
-    // decreaseBtn.textContent = "-"
-
-    // quantityOfItems.classList.add("cart-card__quantity")
-    // quantityOfItems.textContent = quantity
-
-    // increaseBtn.classList.add("cart-card__quantity-control_btn")
-    // increaseBtn.textContent = "+"
-    
-    // itemPrice.classList.add("cart-card__price")
-    // itemPrice.textContent = item.FinalPrice * quantity
-
-    // decreaseBtn.addEventListener("click",()=>{
-    //     quantity --;
-    //     quantityOfItems.textContent = quantity
-    //     itemTotalPrice = quantity * item.FinalPrice
-    // })
-
-    // increaseBtn.addEventListener("click",()=>{
-    //     quantity ++;
-    //     quantityOfItems.textContent = quantity
-    //     itemTotalPrice = quantity * item.FinalPrice
-    // })
-
-    // return {li,itemTotalPrice}
-
     return `
     <li class="cart-card divider">
         <button class="cart-card__remove" type="button" data-id="${item.Id}" aria-label="Remove ${item.Name} from cart">X</button>
@@ -99,9 +17,9 @@ function cartItemTemplate(item) {
         <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     
         <div class="cart-card__quantity-controls">
-            <button class="cart-card__quantity-control_btn">-</button>
-            <p class="cart-card__quantity">qty: 1</p>
-            <button class="cart-card__quantity-control_btn">+</button>
+            <button class="cart-card__quantity-control_btn" data-id="${item.Id}" data-change="subtract">-</button>
+            <p class="cart-card__quantity" data-id="${item.Id}"></p>
+            <button class="cart-card__quantity-control_btn" data-id="${item.Id} data-change="add">+</button>
         </div>
         <p class="cart-card__price">$${item.FinalPrice}</p>
     </li>`;
@@ -124,10 +42,10 @@ export default class ShoppingCart {
         } else {
             renderListWithTemplate(cartItemTemplate, this.listElement, list, "afterbegin", true);
 
-            const total = list.reduce((sum, item) => sum + item.FinalPrice, 0);
-            console.log(total)
-            setCardTotal(total);
+            this.setQuantity();
+            setCardTotal();
             this.addRemoveListeners();
+            this.addChangeQuantity();
         }
     }
 
@@ -146,9 +64,58 @@ export default class ShoppingCart {
         this.renderList(updatedItems);
         updateCartCount();
     }
+    setQuantity() {
+        let elements = document.querySelectorAll(".cart-card__quantity").forEach(el => {
+            let id = el.dataset.id
+            let quantity = sessionStorage.getItem(id)
+            if (!quantity) {
+                sessionStorage.setItem(id, 1)
+            }
+            el.textContent = sessionStorage.getItem(id)
+        })
+    }
+
+    addChangeQuantity() {
+
+        this.listElement.querySelectorAll(".cart-card__quantity-control_btn").forEach((button) => {
+            button.addEventListener("click", (event) => this.handleChangeQuantity(event));
+        });
+    }
+
+    handleChangeQuantity(event) {
+        let currentTarget = event.currentTarget
+        let parent = currentTarget.parentElement
+        let quantityEl = parent.querySelector("p")
+        let operation = currentTarget.textContent
+        let id = quantityEl.dataset.id
+        let quantity = parseInt(sessionStorage.getItem(id))
+
+
+        if (operation == "+") {
+            quantity = quantity + 1
+            quantityEl.textContent = quantity
+            sessionStorage.setItem(id, quantity)
+            setCardTotal()
+
+        } else {
+            quantity = quantity - 1
+            if (quantity > 0) {
+                quantityEl.textContent = quantity
+                sessionStorage.setItem(id, quantity)
+                setCardTotal()
+            }
+        }
+    }
 }
 
-function setCardTotal(total) {
+function setCardTotal() {
+    let list = JSON.parse(localStorage.getItem("so-cart"))
+    const total = list.reduce((sum, item) => {
+        let price = item.FinalPrice
+        let quantity = sessionStorage.getItem(item.Id)
+        return sum + (price * quantity)
+    }, 0)
+
     const cartPriceEl = document.querySelector(".cart-total");
     cartPriceEl.classList.remove("hide");
     cartPriceEl.textContent = `Total: $${total}`;
