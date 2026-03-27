296ab71b098c51c5e1041371772e4cd154396341
Brady Burgener <bradylbsee@gmail.com>
2026-03-27 13:05:33 -0600

    adds if not null statement

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,14 @@ alert.init();
 const newsletterForm = document.getElementById("newsletter-form");
 const newsletterMessage = document.getElementById("newsletter-message");
 
-newsletterForm.addEventListener("submit", (e) => {
+if (newsletterForm !=null){
+  newsletterForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const email = newsletterForm.email.value.trim();
   newsletterMessage.textContent = `Thanks! ${email} has been signed up for our newsletter.`;
   newsletterMessage.hidden = false;
   newsletterForm.reset();
 });
+
+}
+
